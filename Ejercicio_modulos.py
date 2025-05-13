6c0f220ed1f2e0c4001730f95eb375153c4880b4
H-bus13 <hugobustosbarra@gmail.com>
2025-05-13 11:53:06 -0400

    version modular agregada

--- Ejercicio_modulos.py
+++ Ejercicio_modulos.py
@@ -17,16 +17,20 @@ def ejecutar_codigo():
     radio = float(input("Ingrese el radio de la circunferencia: "))
     print()
     respuesta_1 = area_circunferencia(radio)
+
     numero = float(input("Ingrese un número: "))
     print()
     respuesta_2 = raiz_cuadrada(numero)
 
+    print(f"El area de la circunferencia de radio {radio} = {respuesta_1}")
+    print()    
+    print(f"La raíz cuadrada de {numero} = {respuesta_2}") 
+    print()
+
 ejecutar_codigo()
 
 # area = pi * radio**2
-# print(f"El area de la circunferencia de radio {radio} = {area}") 
 
 # numero = float(input("Ingrese un número: "))
 # print()
 # raiz_cuadrada = math.sqrt(numero)
-# print(f"La raíz cuadrada de {numero} = {raiz_cuadrada}") 
