c5f56eed6cdbd2d08e044e8d460e1f37f716678b
H-bus13 <hugobustosbarra@gmail.com>
2025-05-13 11:25:55 -0400

    arreglos

--- Ejercicio_modulos.py
+++ Ejercicio_modulos.py
@@ -3,13 +3,30 @@ import math
 #pi = 3.14
 pi = math.pi
 
-radio = float(input("Ingrese el radio de la circunferencia: "))
-print()
 
-area = pi * radio**2
-print(f"El area de la circunferencia de radio {radio} = {area}") 
+def area_circunferencia(radio):
+    area = pi * radio **2
+    return area
 
-numero = float(input("Ingrese un número: "))
-print()
-raiz_cuadrada = math.sqrt(numero)
-print(f"La raíz cuadrada de {numero} = {raiz_cuadrada}") 
+def raiz_cuadrada(numero):
+    raiz_cuadrada = math.sqrt(numero)
+    return raiz_cuadrada
+
+def ejecutar_codigo():
+    print()
+    radio = float(input("Ingrese el radio de la circunferencia: "))
+    print()
+    respuesta_1 = area_circunferencia(radio)
+    numero = float(input("Ingrese un número: "))
+    print()
+    respuesta_2 = raiz_cuadrada(numero)
+
+ejecutar_codigo()
+
+# area = pi * radio**2
+# print(f"El area de la circunferencia de radio {radio} = {area}") 
+
+# numero = float(input("Ingrese un número: "))
+# print()
+# raiz_cuadrada = math.sqrt(numero)
+# print(f"La raíz cuadrada de {numero} = {raiz_cuadrada}") 
